99e110cf1d9d3c2644bb8e3e5993a2577d432748
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-11-04 10:53:05 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Sport Club Internacional",
+"autor" => "yo",
+"resumen" => "El Sport Club Internacional, conocido popularmente como Internacional, Inter de Porto Alegre o simplemente Inter,9​ es un club multideportivo de la ciudad de Porto Alegre, capital del estado de Rio Grande do Sul, Brasil. Fue fundado el 4 de abril de 1909, por Henrique Poppe Leão. Actualmente participa en el Campeonato Brasileño de Serie A. Sus colores son el rojo y el blanco y sus hinchas son conocidos como Los Colorados.",
 ];
 ?>
