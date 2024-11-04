b5abfb30955a480a4b0472da30c795bb052a8240
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-11-04 10:59:17 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Los peligros del determinismo tecnológico",
+"autor" => "yo",
+"resumen" => "El apogeo tecnológico de herramientas digitales (redes sociales, correos electrónicos, aplicativos, etc.) supone una de las mayores revoluciones del siglo XXI; su implementación ha sacudido la realidad, simplificando brechas, superando barreras geográficas y alterando la manera en la que nos relacionamos. ",
 ];
 ?>
