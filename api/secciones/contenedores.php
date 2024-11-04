2ad0a09b854d21d2609abc42297d7b015286f194
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-11-04 10:54:43 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Presidenta Boluarte: “Avanzamos en fortalecer capacidad de respuesta de nuestras FF.AA.”",
+"autor" => "yo",
+"resumen" => "Lima, 29 de octubre de 2024.- La presidenta de la república, Dina Ercilia Boluarte Zegarra, presentó hoy la aeronave Beechcraft King Air 360C, que formará parte de la flota de la Fuerza Aérea del Perú (FAP) y fortalecerá la capacidad de respuesta de nuestras Fuerzas Armadas ante evacuaciones de emergencia.",
 ];
 ?>
