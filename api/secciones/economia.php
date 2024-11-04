31515f444df1c578e4f45165f8c7b970a1e5b88e
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-11-04 10:56:04 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Mercado de oficinas prime se reactiva: nuevos proyectos se alistan para 2025, ¿dónde?",
+"autor" => "yo",
+"resumen" => "Demanda efectiva de oficinas prime alcanzó un nivel inesperado al cierre del tercer trimestre de 2024, pasando de 6,000 m2 en el primer trimestre a 22,500 m2, según un reporte de Binswanger. ¿Qué se espera para 2025?",
 ];
 ?>
