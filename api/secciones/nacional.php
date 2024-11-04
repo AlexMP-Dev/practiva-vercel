df257d15f02c9e4d9aaabd2ec51652e0a718d95f
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-11-04 10:58:28 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "TVPerú Noticias cumple 11 años informando al país con responsabilidad y veracidad",
+"autor" => "yo",
+"resumen" => "TVPerú Noticias, la señal informativa del Instituto Nacional de Radio y Televisión del Perú (IRTP), celebra once años de operación ininterrumpida, desempeñando un papel crucial en mantener a la ciudadanía informada sobre el acontecer nacional e internacional.",
 ];
 ?>
